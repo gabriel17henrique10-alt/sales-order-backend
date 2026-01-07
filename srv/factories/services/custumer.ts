39dc4d199f39d5c9a2b0ac9b08831ca49bf09f62
gabriel17henrique10-alt <gabriel17henrique10@gmail.com>
2026-01-07 14:37:15 -0300

    feat: add customer's factories

---
+++ srv/factories/services/custumer.ts
@@ -0,0 +1,8 @@
+import { CustumerService } from "../../services/custumer/protocols";
+import { CustumerServiceImpl } from "../../services/custumer/implementation";
+
+const makeCustumerService = (): CustumerService => {
+    return new CustumerServiceImpl();
+}
+
+export const custumerService = makeCustumerService();
