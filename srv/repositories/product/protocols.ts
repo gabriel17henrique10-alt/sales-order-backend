e75dad930e7bf54a48105200afe29a3d13a2c272
gabriel17henrique10-alt <gabriel17henrique10@gmail.com>
2026-01-08 17:09:28 -0300

    feat: add updateStock

--- srv/repositories/product/protocols.ts
+++ srv/repositories/product/protocols.ts
@@ -2,4 +2,5 @@ import { ProductProps, ProductModel } from "srv/models/products";
 
 export interface ProductRepository {
     findByIds(ids: ProductProps["id"][]): Promise<ProductModel[] | null>;
+    updateStock(product: ProductModel): Promise<void>;
 }
