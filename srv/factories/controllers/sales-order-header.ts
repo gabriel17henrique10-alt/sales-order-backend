bc18b4dc26f1d6dbfd53ed3da06af1ee3e5fd0d1
gabriel17henrique10-alt <gabriel17henrique10@gmail.com>
2026-01-08 17:01:53 -0300

    fix: fixing salesOrderHeader calls

--- srv/factories/controllers/sales-order-header.ts
+++ srv/factories/controllers/sales-order-header.ts
@@ -1,10 +1,10 @@
 import { SalesOrderHeader } from "@models/sales";
-import { SalesOerderHeadersControllerImpl } from "srv/controllers/sales-order-header/implementation";
+import { SalesOrderHeadersControllerImpl } from "srv/controllers/sales-order-header/implementation";
 import { SalesOrderHeaderController } from "srv/controllers/sales-order-header/protocols";
 import { salesOrderHeaderService } from "../services/sales-order-header";
 
-export const makeSalesOredrHeaderController = (): SalesOrderHeaderController => {
-    return new SalesOerderHeadersControllerImpl(salesOrderHeaderService);
+export const makeSalesOrderHeaderController = (): SalesOrderHeaderController => {
+    return new SalesOrderHeadersControllerImpl(salesOrderHeaderService);
 }
 
-export const salesOrderHeaderController = makeSalesOredrHeaderController();
+export const salesOrderHeaderController = makeSalesOrderHeaderController();
