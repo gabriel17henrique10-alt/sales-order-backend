b0f865f493a6be25c819f48eafe87bfbab482860
gabriel17henrique10-alt <gabriel17henrique10@gmail.com>
2026-01-22 10:57:03 -0300

    feat: add  either monad approach in findByCustomerIdMethod

--- srv/services/sales-report/protocols.ts
+++ srv/services/sales-report/protocols.ts
@@ -6,5 +6,5 @@ import { AbstractError } from '@/errors';
 
 export interface SalesReportService {
     findByDays(days: number): Promise<Either<AbstractError, SalesReportByDays[]>>;
-    findByCustomerId(customerId: string): Promise<SalesReportByDays[]>;
+    findByCustomerId(customerId: string): Promise<Either<AbstractError, SalesReportByDays[]>>;
 }
