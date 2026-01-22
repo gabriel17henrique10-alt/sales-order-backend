8ab5cf6bcaea6b421570e52361c79b65f6a534d9
gabriel17henrique10-alt <gabriel17henrique10@gmail.com>
2026-01-22 09:43:47 -0300

    chore: change constructor of abstract

--- srv/errors/abstract.ts
+++ srv/errors/abstract.ts
@@ -1,7 +1,7 @@
 export class AbstractError extends Error {
     public code: number;
 
-    constructor(message: string, errorCode: number, stack: string) {
+    constructor(message: string, errorCode: number, stack?: string) {
         super(message);
         this.code = errorCode;
         this.message = message;
