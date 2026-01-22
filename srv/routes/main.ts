5086b37d078d0cce37b233bedec49d111f0a3cda
gabriel17henrique10-alt <gabriel17henrique10@gmail.com>
2026-01-22 09:49:39 -0300

    feat: add Either Monad approach in CAP service

--- srv/routes/main.ts
+++ srv/routes/main.ts
@@ -3,10 +3,10 @@ import '../configs/module-alias';
 
 import { Request, Service } from '@sap/cds';
 
-import { Custumers, SalesOrderHeaders } from '@models/sales';
+import { Customers, SalesOrderHeaders } from '@models/sales';
 
 import { FullRequestParams } from '@/routes/protocols';
-import { custumerController } from '@/factories/controllers/custumer';
+import { customerController } from '@/factories/controllers/customer';
 import { salesOrderHeaderController } from '@/factories/controllers/sales-order-header';
 import { salesReportController } from '@/factories/controllers/sales-report';
 
@@ -28,19 +28,23 @@ export default (service: Service) => {
         }
         request.data.totalAmount = result.totalAmount;
     });
-    service.after('READ', 'Custumers', (custumerList: Custumers, request) => {
-        const result = custumerController.afterRead(custumerList);
+    service.after('READ', 'customers', (customerList: Customers, request) => {
+        const result = customerController.afterRead(customerList);
         if (result.status >= 400) {
             return request.error(result.status, result.data as string);
         }
-        (request as unknown as FullRequestParams<Custumers>).results = result.data as Custumers;
+        (request as unknown as FullRequestParams<Customers>).results = result.data as Customers;
     });
     service.after('CREATE', 'SalesOrderHeaders', async (salesOrderHeaders: SalesOrderHeaders, request: Request) => {
         await salesOrderHeaderController.afterCreate(salesOrderHeaders, request.user);
     });
     service.on('getSalesReportByDays', async (request: Request) => {
         const days = request.data?.days || 7;
-        return salesReportController.findByDays(days);
+        const result = await salesReportController.findByDays(days);
+        if (result.status >= 400) {
+            return request.error(result.status, result.data as string);
+        }
+        return result.data;
     });
     service.on('getSalesReportByCutomerId', async (request: Request) => {
         const [{ id: customerId }] = request.params as unknown as { id: string }[];
